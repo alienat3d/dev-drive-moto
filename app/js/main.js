b8ee74094652e9ded1cc68546aacc0789096683d
alienat3d <psy.alienated@gmail.com>
2022-01-05 09:20:50 +0300

    @media for index.html banner-section

--- app/js/main.js
+++ app/js/main.js
@@ -16,13 +16,23 @@ $(function () {
   });
   // === / [product-page.html] RateYo JQ (star rating) ===
 
+  // === [index.html] Slick Slider JQ ===
   $('.banner-section__slider').slick({
     dots: true,
     prevArrow:
       '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/icons/arrow-left.svg" alt=""></button>',
     nextArrow:
       '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/icons/arrow-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 963,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
+  // === / [index.html] Slick Slider JQ ===
 
   // === [catalogue.html & product-page.html] TABS ===
 
