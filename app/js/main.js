7e6b320cf51ec86321e7dce4b359d30e9b4b0b3f
alienat3d <psy.alienated@gmail.com>
2022-01-11 20:36:06 +0300

    finished w.o. bugfixing

--- app/js/main.js
+++ app/js/main.js
@@ -40,12 +40,14 @@ $(function () {
     e.preventDefault();
 
     $($(this).siblings()).removeClass('tab--active');
-    $($(this).parent().siblings().find('div')).removeClass(
+    $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass(
       'tabs-content--active'
     );
 
     $(this).addClass('tab--active');
     $($(this).attr('href')).addClass('tabs-content--active');
+
+    $('.products-slider').slick('setPosition');
   });
 
   $('.products-item__favorite').on('click', function () {
